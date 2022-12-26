444e06411896d8bdaf40dfc9fa5f89c377a6367b
razeeth11 <razeeth011@gmail.com>
2022-12-26 21:24:57 +0530

    using callback

--- script.js
+++ script.js
@@ -1,16 +1,78 @@
-var time = 10;
-setInterval(()=>{
+// var time = 10;
+// setInterval(()=>{
 
-  var push = document.querySelector('.timer')
-  var show = document.querySelector('.tag')
-  push.innerHTML = time;
-  time--;
+//   var push = document.querySelector('.timer')
+//   var show = document.querySelector('.tag')
+//   push.innerHTML = time;
+//   time--;
 
-  if(time<=-1){
-    clearInterval(setInterval);
-    push.style.display = "none"
-    show.style.display = "block"
-    show.style.color = "white"
-    document.querySelector('body').style.backgroundColor =  '#c54245' 
-  } 
+//   if(time<=-1){
+//     clearInterval(setInterval);
+//     push.style.display = "none"
+//     show.style.display = "block"
+//     show.style.color = "white"
+//     document.querySelector('body').style.backgroundColor =  '#c54245' 
+//   } 
+// },1000)
+
+function timer() {
+
+  setTimeout(()=>{
+    var bodyShow = document.querySelector('.timer')
+    bodyShow.innerHTML = "10"
+                setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "9"
+                setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "8"
+                setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "7"
+                setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "6"
+                 setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "5"
+                 setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "4"
+                 setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "3"
+                 setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "2"
+                 setTimeout(()=>{
+                  var bodyShow = document.querySelector('.timer')
+                  bodyShow.innerHTML = "1"
+                  setTimeout(()=>{
+                    var bodyShow = document.querySelector('.timer')
+                    bodyShow.innerHTML = "0"
+
+                  var push = document.querySelector('.timer')
+                  var show = document.querySelector('.tag')
+                  push.style.display = "none"
+                  show.style.display = "block"
+                  show.style.color = "white"
+                  document.querySelector('body').style.backgroundColor =  '#c54245' 
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
+  },1000)
 },1000)
+
+  
+       
+      } 
+  
+
+
+ let timerCount = timer()
